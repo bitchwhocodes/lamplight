baa625f4033d20711b71e023d64fb1260c84ad25
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-11-25 22:35:55 -0500

    added in number

--- routes/sms.js
+++ routes/sms.js
@@ -8,6 +8,7 @@ var twilio = require('twilio');
 router.post('/', twilio.webhook('44b4af81151ee477a0926f1c3b54ac3f', { host:'lamplight.azurewebsites.net', protocol:'http' }), function(req, res){
  if (req.body.Body == "light it up") {
    console.log("verified number!");
+   res.render('index', { title: 'got the damn text' });
  } else {
    console.log("Wrong number!");
   // sendMessage(res, "Invalid number!");
