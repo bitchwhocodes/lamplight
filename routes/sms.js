a7fe4056c856a208d7d3368b35acf14f810cec2f
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-11-25 22:22:52 -0500

    initial commit

---
+++ routes/sms.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var twilio = require('twilio');
+
+
+
+var express = require('express');
+var router = express.Router();
+/*post*/
+router.post('/', twilio.webhook('44b4af81151ee477a0926f1c3b54ac3f', { host:'foo.herokuapp.com', protocol:'http' }), function(req, res){
+ if (req.body.Body == "light it up") {
+   console.log("verified number!");
+ } else {
+   console.log("Wrong number!");
+  // sendMessage(res, "Invalid number!");
+ }
+ 
+});
+module.exports = router;
+
+/*
+
+function sendMessage(res, message) {
+ var resp = new twilio.TwimlResponse();
+ resp.message(message);
+ res.type('text/xml');
+ res.send(resp.toString());
+}
+
+
+module.exports = router;
+*/
