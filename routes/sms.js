7889a4f98801fffd5a0d60c54a1be55502311d1c
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-11-26 10:06:44 -0500

    added spark io to package.json

--- routes/sms.js
+++ routes/sms.js
@@ -9,7 +9,7 @@ var Spark = require("spark-io");
 /*post*/
 router.post('/', twilio.webhook(process.env.TWILIO, { host:'lamplight.azurewebsites.net', protocol:'http' }), function(req, res){
  if (req.body.Body == "light it up") {
-   console.log("verified number!");
+  
     var resp = new twilio.TwimlResponse();
  	resp.message("shit this works");
  	res.type('text/xml');
@@ -22,11 +22,28 @@ router.post('/', twilio.webhook(process.env.TWILIO, { host:'lamplight.azurewebsi
 
 	board.on("ready", function() {
 		this.pinMode("D0", this.MODES.OUTPUT);
-		var byte = 0;
-  		this.digitalWrite("D0", (byte ^= 1));
+		
+  		this.digitalWrite("D0", 1);
 	});
   //res.render('index', { title: 'got the damn text' });
- } else {
+ } if(req.body.Body == "turn if off "){
+var resp = new twilio.TwimlResponse();
+ 	resp.message("shit this works");
+ 	res.type('text/xml');
+ 	res.send(resp.toString());
+ 	var board = new Spark({
+  		token: process.env.SPARK_TOKEN,
+  		deviceId: process.env.SPARK_ID
+	});
+
+
+	board.on("ready", function() {
+		this.pinMode("D0", this.MODES.OUTPUT);
+		
+  		this.digitalWrite("D0", 0);
+	});
+
+ }else {
    console.log("Wrong number!");
   // sendMessage(res, "Invalid number!");
  }
