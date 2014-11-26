e3faa0144c2eb6ad6cd0e656ebd4a1f0d35ed95b
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-11-26 11:19:22 -0500

    changes

--- routes/sms.js
+++ routes/sms.js
@@ -5,6 +5,11 @@ var Spark = require("spark-io");
 
 
 /*post*/
+
+router.get('/', function(req, res) {
+  res.render('index', { title: 'SMS' });
+});
+
 router.post('/', twilio.webhook(process.env.TWILIO, { host:'lamplight.azurewebsites.net', protocol:'http' }), function(req, res){
  if (req.body.Body == "light it up") {
   
@@ -14,19 +19,22 @@ router.post('/', twilio.webhook(process.env.TWILIO, { host:'lamplight.azurewebsi
  	
  	
  	//resp.message("shit this works turning on"+board);
-	//res.send(resp.toString());
+	
 	var board = new Spark({
   		token: process.env.SPARK_TOKEN,
   		deviceId: process.env.SPARK_ID
 	});
 
+	console.log(board);
+
 board.on("ready", function() {
+	console.log("ready");
 		this.pinMode("D0", this.MODES.OUTPUT);
 		this.digitalWrite("D0",1);
-		resp.send(resp.toString());
+
 		
 	});
-
+res.send(resp.toString());
 	
   //res.render('index', { title: 'got the damn text' });
  }
